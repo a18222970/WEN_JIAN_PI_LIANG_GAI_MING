d4a2a9915fceba7960fe8974135fcb47bea84f41
18222970 <18222970@qq.com>
2022-09-06 08:45:46 +0800

    修改了文本框提示框内容

--- resource.h
+++ resource.h
@@ -18,6 +18,8 @@
 #define IDC_STATIC_02                   1011
 #define IDC_STATIC_3                    1012
 #define IDC_EDIT5                       1013
+#define IDC_STATIC_03                   1014
+#define IDC_STATIC_04                   1015
 
 // Next default values for new objects
 // 
@@ -25,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
